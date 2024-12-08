36f1f785f5c49c889ab535f9d91482ee23556fe6
Michael LeBlanc <leblanc.michael@gmail.com>
2024-12-08 02:37:10 -0500

    Day 8 - part 2

--- src/advent/day08.clj
+++ src/advent/day08.clj
@@ -1,5 +1,6 @@
 (ns advent.day08
   (:require
+   [clojure.set :as set]
    [clojure.string :as str]))
 
 (defn to-sparse [coll]
@@ -18,9 +19,23 @@
 (defn diff [v w] (mapv - v w))
 (defn sum [v w] (mapv + v w))
 (defn node [v w] (sum v (diff v w)))
+(defn gcd [a b] (cond (< b 0) (gcd a (- b)) (> b a) (gcd b a) (= b 0) a :else (gcd b (rem a b))))
+(defn in-bounds [rows cols]
+  (fn [[x y]]
+    (and (>= x 0) (< x rows)
+         (>= y 0) (< y cols))))
 
-(defn in-bounds [rows cols] (fn [[x y]]
-                              (and (>= x 0) (< x rows) (>= y 0) (< y cols))))
+(defn all-nodes [rows cols v w]
+  (if (= v w) #{}
+      (let [d-orig (diff v w)
+            g (apply gcd d-orig)
+            d (mapv #(/ % g) d-orig)
+            v? (in-bounds rows cols)]
+        (->> v (iterate #(sum d %))
+             (take-while v?)
+             set))))
+
+
 (defn part-1 [input] (let [grid (parse-input input)]
                        (->>
                         grid
@@ -32,7 +47,18 @@
                         (filter (in-bounds (count grid) (count (grid 0))))
                         (count))))
 
+(defn part-2 [input] (let [grid (parse-input input)
+                           rows (count grid)
+                           cols (count (grid 0))]
+                       (->> grid
+                            to-sparse
+                            vals
+                            (mapcat (fn [coords] (for [c coords d coords] (all-nodes rows cols c d))))
+                            (reduce set/union)
+                            count)))
+
 (defn -main [& args]
   (let [file-path (first args)
         input (slurp file-path)]
-    (println "Part 1" (part-1 input))))
+    (println "Part 1" (part-1 input))
+    (println "Part 2" (part-2 input))))
